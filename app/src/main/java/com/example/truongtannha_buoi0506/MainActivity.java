a5981f67f0ea63f9744e809e18524b916b1adfb6
Trương Tấn Nhã <33920360+tannha12345663@users.noreply.github.com>
2022-11-02 15:01:00 +0700

    Initial commit

---
+++ app/src/main/java/com/example/truongtannha_buoi0506/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.truongtannha_buoi0506;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
